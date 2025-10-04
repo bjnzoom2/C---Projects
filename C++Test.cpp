e2692a7db988741009fa16f26cece8a38b99160e
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-04 19:44:42 +0800

    Changed minor stuff

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 using namespace std;
 
-namespace radius2{
-    double radius = 7;
-}
-
 int main(){
     double radius = 5;
     const double PI = 3.1415962;
-    double circumference = 2 * PI * radius2::radius;
+    double circumference = 2 * PI * radius;
 
     cout << circumference << "cm" << '\n';
 
