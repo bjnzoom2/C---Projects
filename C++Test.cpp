bd4d4d1f6778c00020cc21220eb45bfc8f231052
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-04 21:00:20 +0800

    More type aliases

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-typedef std::string string_t;
+using string_t = std::string;
 
 int main(){
     double radius = 5;
     const double PI = 3.1415962;
     double circumference = 2 * PI * radius;
 
-    string_t string = "Typedef testing";
+    string_t string = "Type alias testing";
 
     std::cout << string << '\n';
     std::cout << circumference << "cm" << '\n';
