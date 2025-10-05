7fa65886ec0a39a5b4f05f122580c8a83790ae74
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-05 13:45:00 +0800

    Learnt user inputs

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
 int main(){
-    int score = 15;
-    int maxScore = 20;
+    int score;
+    std::cout << "Enter score: ";
+    std::cin >> score;
+
+    int maxScore;
+    std::cout << "Enter max score: ";
+    std::cin >> maxScore;
 
     std::cout << score / (double) maxScore * 100 << "%" << '\n';
 
