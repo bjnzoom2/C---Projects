48093a88fc9b5c73b307a32dfaec610fbcddd986
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-07 17:40:40 +0800

    Learnt some string methods and while loops

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,41 +1,19 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    char tempUnit;
-    std::cout << "Enter temperature unit: ";
-    std::cin >> tempUnit;
-    
-    char newTempUnit;
-    std::cout << "Enter new temperature unit: ";
-    std::cin >> newTempUnit;
-    
-    double temperature;
-    std::cout << "Enter temperature: ";
-    std::cin >> temperature;
-    
-    double newTemperature;
-    
-    bool invalid;
-    
-    if (tempUnit == 'C' && newTempUnit == 'F') {
-        newTemperature = temperature * 1.8 + 32;
-    } else if (tempUnit == 'C' && newTempUnit == 'K') {
-        newTemperature = temperature + 273.15;
-    } else if (tempUnit == 'F' && newTempUnit == 'C') {
-        newTemperature = (temperature - 32) / 1.8;
-    } else if (tempUnit == 'F' && newTempUnit == 'K') {
-        newTemperature = (temperature + 459.67) / 1.8;
-    } else if (tempUnit == 'K' && newTempUnit == 'C') {
-        newTemperature = temperature - 273.15;
-    } else if (tempUnit == 'K' && newTempUnit == 'F') {
-        newTemperature = (temperature - 273.15) * 1.8 + 32;
+    std::string text;
+    std::cout << "Enter text: ";
+    std::getline(std::cin, text);
+
+    int counter = 0;
+    if (!text.empty()) {
+        while (counter < text.length()) {
+            counter += 1;
+            std::cout << counter << ' ';
+        }
     } else {
-        std::cout << "Invalid temperature conversion" << '\n';
-        invalid = true;
-    }
-    
-    if (!invalid) {
-        std::cout << newTemperature << " " << newTempUnit << '\n';   
+        std::cout << "No text detected" << '\n';
     }
 
     return 0;
