98352744dd4661a5cbdd4d85c5d90a55eb4e1bfe
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-08 15:03:35 +0800

    Learnt return keyword

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
-#include <ctime>
+#include <string>
 
-void rollDice(int times) {
-    srand(time(NULL));
-    int dice;
-
-    for (int i = 1; i <= times; i++) {
-        dice = (rand() % 6) + 1;
-        std::cout << dice << '\n';
-    }
+std::string concatString(std::string string1, std::string string2) {
+    return string1 + ' ' + string2;
 }
 
 int main() {
-    rollDice(4);
+    std::string string1;
+    std::string string2;
+
+    do {
+        std::cout << "Enter first string: ";
+        std::getline(std::cin, string1);
+    } while (string1.empty());
+
+    do {
+        std::cout << "Enter second string: ";
+        std::getline(std::cin, string2);
+    } while(string2.empty());
+
+    std::cout << concatString(string1, string2) << '\n';
 
     return 0;
 }
