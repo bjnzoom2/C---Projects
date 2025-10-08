f92eb0eda0c16089c2542070a9621d714bfa6347
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-08 21:15:41 +0800

    Learnt arrays

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,69 +1,11 @@
 #include <iostream>
-#include <iomanip>
-
-void showBalance(double balance) {
-    std::cout << "Your balance is: $" << std::setprecision(2) << std::fixed << balance << '\n';
-}
-
-double deposit() {
-    double amount;
-    std::cout << "Enter deposit amount: $";
-    std::cin >> amount;
-
-    if (amount > 0) {
-        return amount;
-    } else {
-        std::cout << "Invalid amount" << '\n';
-        return 0;
-    }
-
-    return amount;
-}
-
-double withdraw(double balance) {
-    double amount;
-    std::cout << "Enter withdrawal amount: $";
-    std::cin >> amount;
-
-    if (balance >= amount && amount > 0) {
-        return amount;    
-    } else {
-        std::cout << "Insufficient funds" << '\n';
-        return 0;
-    }
-}
 
 int main() {
-    double balance = 0;
-    int choice;
+    double array[] = {100, 12, 1343.123};
 
-    do {
-        std::cout << "Choose an option:" << '\n';
-        std::cout << "1. Show Balance" << '\n';
-        std::cout << "2. Deposit" << '\n';
-        std::cout << "3. Withdraw" << '\n';
-        std::cout << "4. Exit" << '\n';
-
-        std::cin >> choice;
-
-        switch(choice) {
-            case 1:
-                showBalance(balance);
-                break;
-            case 2:
-                balance += deposit();
-                showBalance(balance);
-                break;
-            case 3:
-                balance -= withdraw(balance);
-                showBalance(balance);
-                break;
-            case 4:
-                break;
-            default:
-                std::cout << "Invalid choice" << '\n';
-        }
-    } while (choice != 4);
+    std::cout << array[0] << '\n';
+    std::cout << array[1] << '\n';
+    std::cout << array[2] << '\n';
 
     return 0;
 }
