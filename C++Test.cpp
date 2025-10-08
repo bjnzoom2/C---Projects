397748287aab6bc7c7bb028010aa3f77f49354da
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-08 19:50:00 +0800

    Banking program

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,34 +1,62 @@
 #include <iostream>
-#include <cmath>
+#include <iomanip>
 
-double getVolume(double length) {
-    double volume = pow(length, 3);
-
-    return volume;
+void showBalance(double balance) {
+    std::cout << "Your balance is: $" << std::setprecision(2) << std::fixed << balance << '\n';
 }
 
-double getVolume(double length, double width, double height) {
-    double volume = length * width * height;
+double deposit() {
+    double amount;
+    std::cout << "Enter deposit amount: $";
+    std::cin >> amount;
 
-    return volume;
+    return amount;
+}
+
+double withdraw(double balance) {
+    double amount;
+    std::cout << "Enter withdrawal amount: $";
+    std::cin >> amount;
+
+    if (balance >= amount && amount > 0) {
+        return amount;    
+    } else {
+        std::cout << "Insufficient funds" << '\n';
+        return 0;
+    }
 }
 
 int main() {
-    double length;
-    double width;
-    double height;
+    double balance = 0;
+    int choice;
 
-    std::cout << "Enter length: ";
-    std::cin >> length;
+    do {
+        std::cout << "Choose an option:" << '\n';
+        std::cout << "1. Show Balance" << '\n';
+        std::cout << "2. Deposit" << '\n';
+        std::cout << "3. Withdraw" << '\n';
+        std::cout << "4. Exit" << '\n';
 
-    std::cout << "Enter width: ";
-    std::cin >> width;
+        std::cin >> choice;
 
-    std::cout << "Enter height: ";
-    std::cin >> height;
-
-    std::cout << getVolume(length) << "cm^3" << '\n';
-    std::cout << getVolume(length, width, height) << "cm^3" << '\n';
+        switch(choice) {
+            case 1:
+                showBalance(balance);
+                break;
+            case 2:
+                balance += deposit();
+                showBalance(balance);
+                break;
+            case 3:
+                balance -= withdraw(balance);
+                showBalance(balance);
+                break;
+            case 4:
+                break;
+            default:
+                std::cout << "Invalid choice" << '\n';
+        }
+    } while (choice != 4);
 
     return 0;
 }
