28ce34e3e66939cc4eaf6e5c8c0134b4a9cc54ac
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-09 14:50:19 +0800

    More fill functions

--- C++Test.cpp
+++ C++Test.cpp
@@ -4,7 +4,8 @@ int main() {
     const int SIZE = 50;
 
     int array[SIZE];
-    std::fill(array, array + SIZE, 50);
+    std::fill(array, array + SIZE / 2, 50);
+    std::fill(array + SIZE / 2, array + SIZE, 100);
 
     for (int num : array) {
         std::cout << num << '\n';
