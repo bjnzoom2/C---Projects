811f7ef9f9ff01d279e90ceb1f288293019df6ef
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-09 14:06:11 +0800

    Learnt to sort arrays

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 
-void findElement(double array[], int size, double numChoice) {
-    for (int i = 0; i < size; i++) {
-        if (array[i] == numChoice) {
-            std::cout << numChoice << " has been found at index " << i << " of " << array << '\n';
-            break;
-        } else if (i == size - 1) {
-            std::cout << numChoice << " was not found in " << array << '\n';
+void sort(int array[], int size) {
+    int temp;
+
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+            }
         }
     }
 }
 
 int main() {
-    double array[] = {13, 2, 65, 101};
+    int array[] = {10, 3, 7, 8, 2, 1, 4, 6, 5, 9};
     int size = sizeof(array) / sizeof(array[0]);
 
-    double numChoice;
-    std::cout << "Enter number: ";
-    std::cin >> numChoice;
-
-    findElement(array, size, numChoice);
+    sort(array, size);
+    
+    for (int num : array) {
+        std::cout << num << ' ';
+    }
 
     return 0;
 }
