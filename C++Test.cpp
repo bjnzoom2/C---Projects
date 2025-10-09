7969021cba58a359912f4e7bda95ff14aa8aa781
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-09 20:16:20 +0800

    Learnt memory addresses

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,33 +1,13 @@
 #include <iostream>
 
-const int QUESTIONAMOUNT = 3;
-
-void askQuestions(std::string questions[][QUESTIONAMOUNT], int rows, int columns) {
-    int score = 0;
-    std::string answer;
-
-    for (int i = 0; i <= rows; i++) {
-        std::cout << questions[0][i];
-        std::cin >> answer;
-
-        if (answer == questions[1][i]) {
-            std::cout << "Correct!\n";
-            score++;
-        } else {
-            std::cout << "Incorrect! The correct answer was " << questions[1][i] << '\n';
-        }
-    }
-
-    std::cout << "You score is " << score << " / " << columns << '\n';
-}
-
 int main() {
-    std::string questions[2][QUESTIONAMOUNT] = {{"1. What is 1 + 1: ", "2. What coding language is this: ", "3. What is the speed of light in a vacuum: "},
-                                               {"2", "C++", "299792458"}};
-    int rows = sizeof(questions) / sizeof(questions[0]);
-    int columns = sizeof(questions[0]) / sizeof(questions[0][0]);
+    std::string string = "Hello";
+    int num = 12;
+    bool boolean = true;
 
-    askQuestions(questions, rows, columns);
+    std::cout << &string << '\n';
+    std::cout << &num << '\n';
+    std::cout << &boolean << '\n';
 
     return 0;
 }
