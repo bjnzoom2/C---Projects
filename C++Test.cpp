5e47ca442131a7bf7d7ef530d197c6bf46b87c82
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-09 12:14:00 +0800

    Learnt how to pass array to function

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
-int main() {
-    int array[] = {23, 19, 2006};
-    for (int num : array) {
-        std::cout << num << '\n';
+double sum(double array[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += array[i];
     }
+
+    return sum;
+}
+
+int main() {
+    double array[] = {23, 19, 2006};
+    int size = sizeof(array) / sizeof(int);
+
+    std::cout << sum(array, size) << '\n';
 
     return 0;
 }
