df8312b01c4f66d7c50c94da48e8ae1bcde8f6fc
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-09 20:09:35 +0800

    Quiz game

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 
-int main() {
-    int array2d[][2] = {{1, 2},
-                        {3, 4}};
-    int rows = sizeof(array2d) / sizeof(array2d[0]);
-    int columns = sizeof(array2d[0]) / sizeof(array2d[0][0]);
+const int QUESTIONAMOUNT = 3;
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            std::cout << array2d[i][j] << ' ';
+void askQuestions(std::string questions[][QUESTIONAMOUNT], int rows, int columns) {
+    int score = 0;
+    std::string answer;
+
+    for (int i = 0; i <= rows; i++) {
+        std::cout << questions[0][i];
+        std::cin >> answer;
+
+        if (answer == questions[1][i]) {
+            std::cout << "Correct!\n";
+            score++;
+        } else {
+            std::cout << "Incorrect! The correct answer was " << questions[1][i] << '\n';
         }
-        std::cout << '\n';
     }
+
+    std::cout << "You score is " << score << " / " << columns << '\n';
+}
+
+int main() {
+    std::string questions[2][QUESTIONAMOUNT] = {{"1. What is 1 + 1: ", "2. What coding language is this: ", "3. What is the speed of light in a vacuum: "},
+                                               {"2", "C++", "299792458"}};
+    int rows = sizeof(questions) / sizeof(questions[0]);
+    int columns = sizeof(questions[0]) / sizeof(questions[0][0]);
+
+    askQuestions(questions, rows, columns);
 
     return 0;
 }
