92e96922e7f8ac74726984176c3280efc1b76c00
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-10 16:43:30 +0800

    Credit card number generator

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,19 +1,55 @@
 #include <iostream>
+#include <ctime>
 
-void swap(int &num1, int &num2) {
-    int temp = num1;
-    num1 = num2;
-    num2 = temp;
+std::string generateCardNumber(const int length) {
+    std::string cardNumber = "";
+    srand(time(NULL));
+
+    for (int i = 0; i < length; i++) {
+        char randomNum = rand() % 10 + '0';
+        cardNumber += randomNum;
+    }
+
+    return cardNumber;
+}
+
+int getDigits(const int number) {
+    int result = (number % 10) + (number / 10 % 10);
+    return result;
+}
+
+int sumEven(const std::string cardNumber) {
+    int sum = 0;
+
+    for (int i = cardNumber.size() - 2; i >= 0; i -= 2) {
+        int digit = getDigits(cardNumber[i] * 2);
+        sum += digit;
+    }
+
+    return sum;
+}
+
+int sumOdd(const std::string cardNumber) {
+    int sum = 0;
+
+    for (int i = cardNumber.size() - 1; i >= 0; i--) {
+        int digit = getDigits(cardNumber[i]);
+        sum += digit;
+    }
+
+    return sum;
 }
 
 int main() {
-    int num1 = 5;
-    int num2 = 15;
+    while (true) {
+        std::string cardNumber = generateCardNumber(16);
+        int result = sumEven(cardNumber) + sumOdd(cardNumber);
 
-    swap(num1, num2);
-
-    std::cout << num1 << '\n';
-    std::cout << num2 << '\n';
+        if (result % 10 == 0) {
+            std::cout << cardNumber << " is valid";
+            break;
+        }
+    }
 
     return 0;
 }
