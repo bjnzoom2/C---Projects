4ad0a4b14b784689fb4c4c58099c2258ee5ecfb2
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-10 16:57:39 +0800

    Learnt pointers

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,55 +1,17 @@
 #include <iostream>
-#include <ctime>
-
-std::string generateCardNumber(const int length) {
-    std::string cardNumber = "";
-    srand(time(NULL));
-
-    for (int i = 0; i < length; i++) {
-        char randomNum = rand() % 10 + '0';
-        cardNumber += randomNum;
-    }
-
-    return cardNumber;
-}
-
-int getDigits(const int number) {
-    int result = (number % 10) + (number / 10 % 10);
-    return result;
-}
-
-int sumEven(const std::string cardNumber) {
-    int sum = 0;
-
-    for (int i = cardNumber.size() - 2; i >= 0; i -= 2) {
-        int digit = getDigits(cardNumber[i] * 2);
-        sum += digit;
-    }
-
-    return sum;
-}
-
-int sumOdd(const std::string cardNumber) {
-    int sum = 0;
-
-    for (int i = cardNumber.size() - 1; i >= 0; i--) {
-        int digit = getDigits(cardNumber[i]);
-        sum += digit;
-    }
-
-    return sum;
-}
 
 int main() {
-    while (true) {
-        std::string cardNumber = generateCardNumber(16);
-        int result = sumEven(cardNumber) + sumOdd(cardNumber);
+    int num = 12;
+    int *pNum = &num;
 
-        if (result % 10 == 0) {
-            std::cout << cardNumber << " is valid";
-            break;
-        }
-    }
+    int array[] = {1, 3, 5, 7, 9};
+    int *pArray = array;
+
+    std::cout << pNum << '\n';
+    std::cout << *pNum << '\n';
+
+    std::cout << pArray << '\n';
+    std::cout << *pArray << '\n';
 
     return 0;
 }
