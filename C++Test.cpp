2e5fb1b71d4b402d765f0fcb5a67b1caff4eddb5
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-10 21:32:01 +0800

    Tic tac toe

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,16 +1,142 @@
 #include <iostream>
+#include <ctime>
+
+void drawBoard(char *spaces) {
+    std::cout << '\n';
+    std::cout << "       |       |       " << '\n';
+    std::cout << "   " << spaces[0] << "   |   " << spaces[1] << "   |   " << spaces[2] << "   " << '\n';
+    std::cout << "_______|_______|_______" << '\n';
+    std::cout << "       |       |       " << '\n';
+    std::cout << "   " << spaces[3] << "   |   " << spaces[4] << "   |   " << spaces[5] << "   " << '\n';
+    std::cout << "_______|_______|_______" << '\n';
+    std::cout << "       |       |       " << '\n';
+    std::cout << "   " << spaces[6] << "   |   " << spaces[7] << "   |   " << spaces[8] << "   " << '\n';
+    std::cout << "       |       |       " << '\n';
+}
+
+void playerMove(char *spaces, char player) {
+    int number;
+
+    do {
+        std::cout << "Enter position number (1 to 9): ";
+        std::cin >> number;
+
+        if (spaces[number - 1] == ' ') {
+            spaces[number - 1] = player;
+            break;
+        }
+    } while (number > 9 || number < 1);
+    
+    drawBoard(spaces);
+}
+
+void computerMove(char *spaces, char computer) {
+    srand(time(NULL));
+
+    int number;
+
+    while (true) {
+        number = rand() % 9;
+
+        if (spaces[number] == ' ') {
+            spaces[number] = computer;
+            break;
+        } 
+    }
+
+    drawBoard(spaces);
+}
+
+char checkWinner(char *spaces, char player, char computer) {
+    if ((spaces[0] == player && spaces[1] == player && spaces[2] == player) || (spaces[3] == player && spaces[4] == player && spaces[5] == player)
+        || (spaces[6] == player && spaces[7] == player && spaces[8] == player) || (spaces[0] == player && spaces[3] == player && spaces[6] == player)
+        || (spaces[1] == player && spaces[4] == player && spaces[7] == player) || (spaces[2] == player && spaces[5] == player && spaces[8] == player)
+        || (spaces[0] == player && spaces[4] == player && spaces[8] == player) || (spaces[2] == player && spaces[4] == player && spaces[6] == player)) {
+        
+        return player;
+    } else if ((spaces[0] == computer && spaces[1] == computer && spaces[2] == computer) || (spaces[3] == computer && spaces[4] == computer && spaces[5] == computer)
+        || (spaces[6] == computer && spaces[7] == computer && spaces[8] == computer) || (spaces[0] == computer && spaces[3] == computer && spaces[6] == computer)
+        || (spaces[1] == computer && spaces[4] == computer && spaces[7] == computer) || (spaces[2] == computer && spaces[5] == computer && spaces[8] == computer)
+        || (spaces[0] == computer && spaces[4] == computer && spaces[8] == computer) || (spaces[2] == computer && spaces[4] == computer && spaces[6] == computer)) {
+
+        return computer;
+    } else {
+        return ' ';
+    }
+}
+
+bool checkTie(char *spaces, double &occupied) {
+    for (int i = 0; i < sizeof(spaces) / sizeof(spaces[0]); i++) {
+            if (spaces[i] != ' ') {
+                occupied += 0.6;
+            }
+        }
+
+    if (occupied >= 9) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 int main() {
-    int *pointer = nullptr;
-    int num = 12;
+    char spaces[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 
-    pointer = &num;
+    bool running = true;
 
-    if (pointer != nullptr) {
-        std::cout << pointer << '\n';
-        std::cout << *pointer << '\n';
-    } else {
-        std::cout << "Pointer is a null pointer\n";
+    char player;
+    char computer;
+    std::cout << "Enter player icon (X / O): ";
+    std::cin >> player;
+
+    switch(player) {
+        case 'X':
+            computer = 'O';
+            break;
+        case 'O':
+            computer = 'X';
+            break;
+        default:
+            std::cout << "Invalid icon" << '\n';
+    }
+
+    drawBoard(spaces);
+
+    while (running) {
+        double occupied = 0;
+        bool tie;
+        char winner;
+        playerMove(spaces, player);
+        winner = checkWinner(spaces, player, computer);
+        tie = checkTie(spaces, occupied);
+
+        if (winner == player) {
+            std::cout << "Player has won" << '\n';
+            break;
+        } else if (winner == computer) {
+            std::cout << "Computer has won" << '\n';
+            break;
+        }
+
+        if (tie) {
+            break;
+        }
+
+        computerMove(spaces, computer);
+        winner = checkWinner(spaces, player, computer);
+        tie = checkTie(spaces, occupied);
+
+        if (winner == player) {
+            std::cout << "Player has won" << '\n';
+            break;
+        } else if (winner == computer) {
+            std::cout << "Computer has won" << '\n';
+            break;
+        }
+
+        if (tie) {
+            break;
+        }
     }
 
     return 0;
