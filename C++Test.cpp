a2dcc3ed3b8e0069403f62984377364bd3d377b4
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-11 20:52:12 +0800

    Fixed object class

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,37 +1,53 @@
 #include <iostream>
 #include <windows.h>
 
+int counter = 0;
+
 class Object {
     public:
         double mass = 1; // kg
         double speed = 0; // m/s
         double acceleration = 0; // m/s
 
-        void accelerate(double aSpeed) {
-            acceleration += aSpeed;
+        void accelerate(double a) {
+            acceleration = a;
         }
 
-        double getForce() {
+        double getAccelerationForce() {
             return mass * acceleration;
         }
 
+        double getTotalForce() {
+            return getAccelerationForce() * counter;
+        }
+
         void run() {
+            counter++;
             speed += acceleration;
             std::cout << "Mass: " << mass << " kg\n";
-            std::cout << "Acceleration: " << acceleration << " m/s\n";
+            std::cout << "Acceleration: " << acceleration << " m/s^2\n";
             std::cout << "Speed: " << speed << " m/s\n";
-            std::cout << "Force: " << getForce() << " N\n\n"; 
+            std::cout << "Acceleration Force: " << getAccelerationForce() << " N\n";
+            std::cout << "Total Force: " << getTotalForce() << " N\n\n";
         }
 };
 
 int main() {
     bool running = true;
     Object ball;
-    ball.mass = 5;
+
+    ball.mass;
+    std::cout << "Enter mass: ";
+    std::cin >> ball.mass;
+
+    double a;
+    std::cout << "Enter acceleration: ";
+    std::cin >> a;
+    std::cout << '\n';
 
     while (running) {
+        ball.accelerate(a);
         ball.run();
-        ball.accelerate(1);
         Sleep(1000);
     }
 
