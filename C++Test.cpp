18982b9d2f6f2c79b7c13c24118ccd68a543d1d2
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-19 20:41:42 +0800

    Learnt STL containers

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
+
+#include <array>
 #include <vector>
+#include <list>
 
 int main() {
-    std::vector<int> vector1 = {1, 2, 3, 4, 5};
-    vector1.insert(vector1.begin() + 1, 6);
-    vector1.erase(vector1.begin() + 3);
+    std::array<int, 5> array = {1, 2, 3, 4, 5};
+    std::cout << array.size() << '\n';
 
-    for (int value : vector1) {
-        std::cout << value << ' ';
+    std::vector<int> vector = {1, 2, 3, 4, 5};
+    vector.push_back(6);
+
+    for (int i : vector) {
+        std::cout << i << ' ';
     }
+    std::cout << '\n';
 
+    std::list<int> list = {1, 2, 3, 4, 5};
+    for (int i : list) {
+        std::cout << &i << ' ';
+    }
+    list.push_back(6);
+    std::cout << '\n';
+    for (int i : list) {
+        std::cout << &i << ' ';
+    }
     std::cout << '\n';
 
     return 0;
