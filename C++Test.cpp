08ab1886a0a0885d71326993e07b2be52cf89e7b
bjnzoom2 <bjnzoom2@gmail.com>
2025-11-12 20:51:25 +0800

    Learnt operator overloading

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 
-class Shape {
-    public:
-    virtual void print() = 0;
-};
+class Vec2 {
+    private:
+    float x, y;
 
-class Triangle : public Shape{
     public:
-    void print() {std::cout << "Triangle\n";}
+    Vec2(float _x, float _y) : x(_x), y(_y) {}
+    float getX() {return x;}
+    float getY() {return y;}
+    Vec2 operator+(Vec2& other) {
+        return Vec2(x + other.x, y + other.y);
+    }
 };
 
 int main() {
-    Triangle tri;
-    tri.print();
+    Vec2 vector1(10, 5);
+    Vec2 vector2(-2, 8);
+
+    Vec2 vec = vector1 + vector2;
+
+    std::cout << vec.getX() << ' ' << vec.getY() << '\n';
 
     return 0;
 }
