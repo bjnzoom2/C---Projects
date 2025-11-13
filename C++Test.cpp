43d9017b2004488c4e6aae8015804590d298841a
bjnzoom2 <bjnzoom2@gmail.com>
2025-11-13 13:34:53 +0800

    Learnt class templates

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 
+template <typename T, typename U>
+
 class Vec2 {
     private:
-    float x, y;
+    T x;
+    U y;
 
     public:
-    Vec2(float _x, float _y) : x(_x), y(_y) {}
-    float getX() {return x;}
-    float getY() {return y;}
+    Vec2(T _x, U _y) : x(_x), y(_y) {}
+    T getX() {return x;}
+    U getY() {return y;}
     Vec2 operator+(Vec2& other) {
         return Vec2(x + other.x, y + other.y);
     }
 };
 
 int main() {
-    Vec2 vector1(10, 5);
-    Vec2 vector2(-2, 8);
+    Vec2 vector1(10, 5.8);
+    Vec2<int, double> vector2(-2, 8);
 
     Vec2 vec = vector1 + vector2;
 
