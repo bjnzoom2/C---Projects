6dd6802f787827db82900e7787785984db99afee
bjnzoom2 <bjnzoom2@gmail.com>
2025-11-13 16:25:43 +0800

    Learnt metaprogramming type traits

--- C++Test.cpp
+++ C++Test.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 
-template <typename T, typename U>
+struct Vec3 {
+    float x, y, z;
+};
 
-class Vec2 {
-    private:
-    T x;
-    U y;
-
-    public:
-    Vec2(T _x, U _y) : x(_x), y(_y) {}
-    T getX() {return x;}
-    U getY() {return y;}
-    Vec2 operator+(Vec2& other) {
-        return Vec2(x + other.x, y + other.y);
-    }
+namespace std {
+    template <typename T>
+    struct is_vec3 : std::false_type {};
+    template <>
+    struct is_vec3<Vec3> : std::true_type {};
 };
 
 int main() {
-    Vec2 vector1(10, 5.8);
-    Vec2<int, double> vector2(-2, 8);
+    int x = 5;
+    float y = 0.1f;
+    std::cout << std::is_floating_point_v<decltype(x)> << '\n';
+    std::cout << std::is_floating_point_v<decltype(y)> << '\n';
 
-    Vec2 vec = vector1 + vector2;
-
-    std::cout << vec.getX() << ' ' << vec.getY() << '\n';
+    Vec3 vec3;
+    std::cout << std::is_vec3<int>::value << '\n';
+    std::cout << std::is_vec3<decltype(vec3)>::value << '\n';
 
     return 0;
 }
